45cbf3d39fbb9c527cf33201fd73e50a032af1db
AndersonPGS <andersonpgs2001@hotmail.com>
2021-09-11 11:11:13 -0300

    removed card import (not used)

--- web/src/components/Promotion/Search/Search.js
+++ web/src/components/Promotion/Search/Search.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 
 import "./Search.css"
 
-import PromotionCard from "../Card/Card";
 import PromotionList from "../List/List";
 
 const PromotionSearch = () => {
